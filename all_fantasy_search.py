0bbf2b34cb777766e18b5932d03dbab6328221e5
Mike Peralta <michaelperalta1@gmail.com>
2023-05-14 18:32:04 -0400

    Update all_fantasy_search.py

--- all_fantasy_search.py
+++ all_fantasy_search.py
@@ -27,42 +27,6 @@ if 'filters_selected' not in st.session_state:
 if 'filter_remove_list' not in st.session_state:
     st.session_state.filter_remove_list = [] 
 
-def filter_container():
-    # st.sidebar.markdown('---')
-    st.sidebar.write('Select Filter(s)')
-    cols = st.sidebar.columns(3)
-    
-    for i in st.session_state.filters_selected:
-        if i not in st.session_state.filter_remove_list:
-            operator = cols[0].selectbox('Select Filter',options=filter_options,key='filter_%s' % i,label_visibility="collapsed")
-
-            if operator in ['contains','does not contain']:
-                cols[1].text_input('Input Value',placeholder='Input Value',key='filter_value_%s' % i,label_visibility="collapsed")
-                cols[2].button('Remove',key='remove_filter_%s' % i,on_click=remove,kwargs={'session_state_attribute':'filter_remove_list','session_state_value':i})
-            
-    
-    st.sidebar.button('Add Filter',on_click=increment,kwargs={'session_state_attribute':'filter_count','session_state_attribute_list':'filters_selected'})
-
-filter_container()
-
-def get_filter_selections(filter_select_list,filter_remove_list,input_keys,default_states):
-    
-    selections = []
-    
-    for s in st.session_state[filter_select_list]:
-        if s not in st.session_state[filter_remove_list]:
-            
-            filter_operator = st.session_state['filter_%s' % s]
-            
-            if filter_operator not in default_states:
-                filter_value = st.session_state['filter_value_%s' % s]
-                if filter_value not in default_states and filter_value != '' and filter_value != []:
-                    selections.append([filter_operator,filter_value])
-                    
-    return selections
-
-selected_filters = get_filter_selections('filters_selected','filter_remove_list',['filter','filter_value'],['Select Column','Select Filter','Select Value'])
-
 url = 'https://itunes.apple.com/search?term=All Fantasy Everything&media=podcast'
 
 r = requests.get(url)
@@ -76,10 +40,43 @@ episodes = episodes['rss']['channel']['item']
 
 episodes = pandas.DataFrame(episodes)
 
-cols = st.sidebar.columns(2)
+with st.expander('Filters'):
 
-# operator = cols[0].selectbox('Operator',options=['contains','does not contain'],label_visibility='hidden')
-# search_value = cols[1].text_input('Search Value',label_visibility='hidden')
+    def filter_container():
+        # st.sidebar.markdown('---')
+        st.write('Select Filter(s)')
+        cols = st.columns(3)
+        
+        for i in st.session_state.filters_selected:
+            if i not in st.session_state.filter_remove_list:
+                operator = cols[0].selectbox('Select Filter',options=filter_options,key='filter_%s' % i,label_visibility="collapsed")
+    
+                if operator in ['contains','does not contain']:
+                    cols[1].text_input('Input Value',placeholder='Input Value',key='filter_value_%s' % i,label_visibility="collapsed")
+                    cols[2].button('Remove',key='remove_filter_%s' % i,on_click=remove,kwargs={'session_state_attribute':'filter_remove_list','session_state_value':i})
+                
+        
+        st.button('Add Filter',on_click=increment,kwargs={'session_state_attribute':'filter_count','session_state_attribute_list':'filters_selected'})
+    
+    filter_container()
+
+    def get_filter_selections(filter_select_list,filter_remove_list,input_keys,default_states):
+        
+        selections = []
+        
+        for s in st.session_state[filter_select_list]:
+            if s not in st.session_state[filter_remove_list]:
+                
+                filter_operator = st.session_state['filter_%s' % s]
+                
+                if filter_operator not in default_states:
+                    filter_value = st.session_state['filter_value_%s' % s]
+                    if filter_value not in default_states and filter_value != '' and filter_value != []:
+                        selections.append([filter_operator,filter_value])
+                        
+        return selections
+    
+    selected_filters = get_filter_selections('filters_selected','filter_remove_list',['filter','filter_value'],['Select Column','Select Filter','Select Value'])
 
 if len(selected_filters) > 0:
     
@@ -93,13 +90,13 @@ if len(selected_filters) > 0:
             episodes = episodes[['title','description','itunes:episode','pubDate','enclosure']]
             episodes.sort_values(by=['itunes:episode'])
             
-    st.sidebar.subheader(str(len(episodes)) + ' episodes match your criteria')
+    st.subheader(str(len(episodes)) + ' episodes match your criteria')
     
-    select_random_episode = st.sidebar.checkbox('Select Random Episode',value=True)
+    select_random_episode = st.checkbox('Select Random Episode',value=True)
         
     if select_random_episode:
     
-        random_episode = st.sidebar.button('Select Again')
+        random_episode = st.button('Select Again')
         random_episode = True
         
         if random_episode:
@@ -117,7 +114,7 @@ if len(selected_filters) > 0:
             st.audio(episode_link['@url'])
             
     else:
-        selected_episode = st.sidebar.selectbox('Select Episode',options=episodes['title'])
+        selected_episode = st.selectbox('Select Episode',options=episodes['title'])
         episodes = episodes.query('title == "%s"' % selected_episode)
         
         episode_name = episodes['title'].values.tolist()[0]
